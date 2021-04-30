5c379400f6a550e7ee8ceeb2090ef926fa641e4c
Silvio Sotelo <silvio.sotelo@century.com.py>
2021-04-30 15:49:12 -0400

    ...

--- libs/app.php
+++ libs/app.php
@@ -6,9 +6,9 @@ class App{
         $url = $_GET['url'];
         $url = rtrim($url, '/');
         $url = explode('/', $url);
-        //var_dump($url);
+        //var_dump($url); // comentario random
 
-        $archivoController = 'controllers/' . $url[0] . '.php';
+        $archivoController = '/controllers/' . $url[0] . '.php';
 
         require_once '$archivoController';
         $controller = new $url[0];
