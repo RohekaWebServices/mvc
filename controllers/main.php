1871ecd73f544a990250824f0e7de5be0033424e
Silvio Sotelo <silvio.sotelo@century.com.py>
2021-04-30 15:39:39 -0400

    modificaciones

---
+++ controllers/main.php
@@ -0,0 +1,13 @@
+<?php
+
+class Main{
+
+    function __construct(){
+        echo "<p>Nuevo Controlador Main</p>";
+
+    }
+
+}
+
+
+?>
--- libs/app.php
+++ libs/app.php
@@ -6,7 +6,12 @@ class App{
         $url = $_GET['url'];
         $url = rtrim($url, '/');
         $url = explode('/', $url);
-        var_dump($url);
+        //var_dump($url);
+
+        $archivoController = 'controllers/' . $url[0] . '.php';
+
+        require_once '$archivoController';
+        $controller = new $url[0];
     }
 }
 ?>
